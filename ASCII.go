722cc428e331053173c9e20d734f8f9cd2b0120f
William Smith <75560670+Tykhist@users.noreply.github.com>
2022-05-15 14:22:23 -0700

    Update ASCII.go
    Added comments

--- ASCII.go
+++ ASCII.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+  // Module for printing with Println method
   "fmt"
+  // Module for current time with Now method
   t "time"
 )
 
 func main () {
+  // Prints art of Golang Gopher, a tree and a dog, and then the time of printing
   fmt.Println("      `.-::::::-.`                            ")
   fmt.Println("  .:-::::::::::::::-:.                        ")
   fmt.Println("  `_:::    ::    :::_`                        ")
@@ -18,6 +21,6 @@ func main () {
   fmt.Println("   `::::::::::::::::`     |===|    o'')}____//")
   fmt.Println("    .::::::::::::::.      |===|     `_/      )")
   fmt.Println("      oO:::::::Oo        /=====\\     (_(_/-(_/")
-
+  // Gives current time
   fmt.Println(t.Now())
 }
