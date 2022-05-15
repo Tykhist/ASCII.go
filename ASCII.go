6efac57d571ee224993b8cc6a30f3340e30e2d79
William Smith <75560670+Tykhist@users.noreply.github.com>
2022-05-15 14:16:51 -0700

    Create ASCII.go

---
+++ ASCII.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+  "fmt"
+  t "time"
+)
+
+func main () {
+  fmt.Println("      `.-::::::-.`                  ")
+  fmt.Println("  .:-::::::::::::::-:.              ")
+  fmt.Println("  `_:::    ::    :::_`              ")
+  fmt.Println("   .:( ^   :: ^   ):.       ^         ")
+  fmt.Println("   `:::   (..)   :::.      (=)           ")
+  fmt.Println("   `:::::::UU:::::::`     (===)           ")
+  fmt.Println("   .::::::::::::::::.    (=====)           ")
+  fmt.Println("   O::::::::::::::::O   (=======)           ")
+  fmt.Println("   -::::::::::::::::-     |===|      __      _")
+  fmt.Println("   `::::::::::::::::`     |===|    o'')}____//")
+  fmt.Println("    .::::::::::::::.      |===|     `_/      )")
+  fmt.Println("      oO:::::::Oo        /=====\\     (_(_/-(_/")
+
+  fmt.Println(t.Now())
+}
